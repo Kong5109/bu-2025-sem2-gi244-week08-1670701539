66a1a7acdbf5894a744c6e0942248b2d45379aca
Kong5109 <kong8071.kk@gmail.com>
2026-03-06 16:59:19 +0700

    Week 8 Complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,12 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10f;
     public float gravityMultiplier = 1f;
     public bool gameOver = false;
+    public Animator animator;
+    public ParticleSystem fxDirt;
+    public GameObject fxExplosionPrefab;
+    public AudioClip audioJump;
+    public AudioClip audioCrash;
+
     private Rigidbody rb;
     private InputAction jumpAction;
     // 5.8 add audio source variable to play crash sound
@@ -17,6 +23,7 @@ public class PlayerController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         jumpAction = InputSystem.actions.FindAction("Jump");
+        jumpAction?.Enable();
 
         // 5.8 get audio source component, if not exist, add one
         audioSource = GetComponent<AudioSource>();
@@ -30,6 +37,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         Physics.gravity *= gravityMultiplier;
+        animator.SetFloat("Speed_f", 1f);
     }
 
     // Update is called once per frame
@@ -44,6 +52,9 @@ public class PlayerController : MonoBehaviour
         {
             rb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
             isOnGround = false;
+            animator.SetTrigger("Jump_trig");
+            fxDirt.Stop();
+            audioSource.PlayOneShot(audioJump);
         }
     }
 
@@ -52,11 +63,17 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            fxDirt.Play();
         }
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over!");
             gameOver = true;
+            animator.SetBool("Death_b", true);
+            animator.SetInteger("DeathType_int", 1);
+            fxDirt.Stop();
+            Instantiate(fxExplosionPrefab, transform.position, Quaternion.identity);
+            audioSource.PlayOneShot(audioCrash);
         }
     }
 }
